e6c32e6259159181243b7b360f4db5547878a294
Eugene Manuilov <eugene.manuilov@10up.com>
2017-11-18 12:11:08 +0200

    Updated request method to properly process it and log requests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 class NovaPoshta {
-	constructor(apiKey = '') {
-		this.apiKey = apiKey;
-		this.endpoint = 'https://api.novaposhta.ua/v2.0/json/';
+
+	constructor(options = {}) {
+		this.apiKey = options.apiKey || '';
+		this.endpoint = options.endpoint || 'https://api.novaposhta.ua/v2.0/json/';
+		this.logger = options.logger || false;
+	}
+
+	log(message, params = {}, type = 'info') {
+		if (this.logger) {
+			this.logger.log(type, message, params);
+		}
 	}
 
 	request(params) {
@@ -11,11 +19,36 @@ class NovaPoshta {
 			body: JSON.stringify(Object.assign({}, params, { apiKey: self.apiKey })),
 		};
 
-		return new Promise((success, error) => {
-			fetch(self.endpoint, args).catch(error).then((response) => {
-				response.json().catch(error).then((json) => {
-					success(json, response);
-				});
+		return new Promise((resolve, reject) => {
+			const fetchPromise = fetch(self.endpoint, args);
+
+			const onSuccess = (json, data) => {
+				self.log(`NovaPoshta::${params.calledMethod} request executed`, { params, response: json }, 'info');
+				resolve(json, data);
+			};
+
+			const onError = (error, data) => {
+				self.log(`NovaPoshta::${params.calledMethod} request failed: ${error}`, params, 'error');
+				reject(error, data);
+			};
+
+			fetchPromise.catch(error => onError(error, null));
+			fetchPromise.then((response) => {
+				if (response.ok) {
+					const jsonPromise = response.json();
+
+					jsonPromise.catch(error => onError(error, null));
+					jsonPromise.then((json) => {
+						if (json.success) {
+							onSuccess(json, response);
+						} else {
+							const error = json.errors[0] || 'Unexpected error';
+							onError(error, json);
+						}
+					});
+				} else {
+					onError(response.statusText, null);
+				}
 			});
 		});
 	}
@@ -35,6 +68,7 @@ class NovaPoshta {
 			methodProperties: params,
 		});
 	}
+
 }
 
 export default NovaPoshta;
